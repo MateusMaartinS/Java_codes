aa2e3e89338aa24961c46cc897b9b2218a4e9ffc
Mateus Martins S <108599241+MateusMaartinS@users.noreply.github.com>
2024-02-28 21:43:43 -0300

    Add files via upload
    Aula 1

---
+++ HelloWorld/src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author aluno
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        new FrameHelloWorld().setVisible(true);
+        
+    }
+}
