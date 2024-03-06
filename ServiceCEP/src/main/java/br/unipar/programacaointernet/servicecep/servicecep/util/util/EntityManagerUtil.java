0645003ff59198e32e17d8b1e176ad14ffa3b5a1
Mateus Martins S <108599241+MateusMaartinS@users.noreply.github.com>
2024-03-05 21:24:09 -0300

    Add files via upload

---
+++ ServiceCEP/src/main/java/br/unipar/programacaointernet/servicecep/servicecep/util/util/EntityManagerUtil.java
@@ -0,0 +1,42 @@
+package br.unipar.programacaointernet.servicecep.servicecep.util.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EntityManagerUtil {
+    private static EntityManagerFactory emf; // Executa uma vez só durante toda a aplicação
+
+    private static EntityManager em; //cada requisição
+
+    private EntityManagerUtil(){}
+
+    public static EntityManagerFactory getEntityManagerFactory(){
+        if (emf == null){
+            emf = Persistence.createEntityManagerFactory("HibernateMaven");
+            System.out.println("Conexão aberta");
+        }
+        return emf;
+    }
+    public static EntityManager getManager(){
+        if (em == null || !em.isOpen() ){
+            em = emf.createEntityManager();
+            System.out.println("entity manager aberta");
+        }
+        return em;
+    }
+
+    public static void closeEntityManagerFactory(){
+        if (emf != null && emf.isOpen()){
+            emf.close();
+            System.out.println("Conexão fechada");
+        }
+    }
+    public static void main (String[] args){
+        try{
+            EntityManagerUtil.getEntityManagerFactory();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
