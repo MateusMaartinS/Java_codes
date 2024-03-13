ab40cf31b98ce69c03d18e0153f0ceb22257352b
Mateus Martins S <108599241+MateusMaartinS@users.noreply.github.com>
2024-03-12 21:34:16 -0300

    Add files via upload

--- ServiceCEP/src/main/java/br/unipar/programacaointernet/servicecep/servicecep/util/EntityManagerUtil.java
+++ ServiceCEP/src/main/java/br/unipar/programacaointernet/servicecep/servicecep/util/EntityManagerUtil.java
@@ -5,38 +5,33 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class EntityManagerUtil {
-    private static EntityManagerFactory emf; // Executa uma vez só durante toda a aplicação
 
-    private static EntityManager em; //cada requisição
-
-    private EntityManagerUtil(){}
-
+    private static EntityManagerFactory emf;//  uma vez só durante toda a aplicação
+    private static EntityManager em;//  cada requisição chama
+    private EntityManagerUtil (){    }
     public static EntityManagerFactory getEntityManagerFactory(){
         if (emf == null){
-            emf = Persistence.createEntityManagerFactory("HibernateMaven");
-            System.out.println("Conexão aberta");
+            emf = Persistence.createEntityManagerFactory("HibernateMaven");//  deve estar igual o do xml
+            System.out.println("conexão aberta");
         }
         return emf;
     }
-    public static EntityManager getManager(){
-        if (em == null || !em.isOpen() ){
+    public  static EntityManager getManager(){
+        if (emf == null){
+            getEntityManagerFactory();
+        }
+        if (em == null || !em.isOpen()){
             em = emf.createEntityManager();
             System.out.println("entity manager aberta");
         }
+
         return em;
     }
-
-    public static void closeEntityManagerFactory(){
-        if (emf != null && emf.isOpen()){
+    public static void closeEntityManagerFactory() {
+        if (emf != null && emf.isOpen()) {
             emf.close();
-            System.out.println("Conexão fechada");
-        }
-    }
-    public static void main (String[] args){
-        try{
-            EntityManagerUtil.getEntityManagerFactory();
-        }catch (Exception e){
-            System.out.println(e);
+            System.out.println("conecxão fechada");
+
         }
     }
 }
