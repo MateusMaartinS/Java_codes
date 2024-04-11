6ca41281ceec8700a6f88b8f109d9d0657baa8fd
Mateus Martins S <108599241+MateusMaartinS@users.noreply.github.com>
2024-04-10 22:31:38 -0300

    Add files via upload

---
+++ app_OS/src/main/java/com/mycompany/app_os/App_OS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app_os;
+
+/**
+ *
+ * @author aluno
+ */
+public class App_OS {
+
+    public static void main(String[] args) {
+        new Frame_login().setVisible(true);
+        
+    }
+    
+    
+    
+}
---
+++ app_OS/src/main/java/com/mycompany/app_os/Controller.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.app_os;
+
+import com.mycompany.app_os.Model.Class_cliente;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aluno
+ */
+public class Controller {
+    
+    public static ArrayList<Class_cliente> listaCliente;
+}
