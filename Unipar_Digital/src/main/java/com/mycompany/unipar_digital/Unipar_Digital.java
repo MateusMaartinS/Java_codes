25b5af62fc73995431f829a8076b1ba143a5e4a3
Mateus Martins S <108599241+MateusMaartinS@users.noreply.github.com>
2024-06-03 19:29:57 -0300

    Add files via upload

---
+++ Unipar_Digital/src/main/java/com/mycompany/unipar_digital/Unipar_Digital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unipar_digital;
+
+/**
+ *
+ * @author aluno
+ */
+public class Unipar_Digital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
